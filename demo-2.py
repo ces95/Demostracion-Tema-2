97489de29f1b783cf575836a97e6880c06160ad0
ces95 <35697127+ces95@users.noreply.github.com>
2021-03-01 21:30:29 -0400

    Update demo-2.py

--- demo-2.py
+++ demo-2.py
@@ -9,3 +9,42 @@ Salma Marmol - 108
 # Librerias
 import os
 
+#VARIABLES GLOBALES
+
+ans = ''
+
+
+#FUNCIONES
+
+
+
+#INICIO DEL MENU DEL PROGRAMA
+while ans :
+    print(
+        """
+        Menu de Inicio
+        --- Seleccione una de las siguiente opciones con los numeros ---
+        [1] Encriptar un archivo 
+        [2] Desencriptar un archivo 
+        [3] Salir 
+
+        """)
+    
+    ans = input("Que desea hacer? ")
+    
+    if ans=="1":
+        os.system('cls')
+        print(
+        """
+        Menu de Encriptado
+        --- Seleccione una de las siguiente opciones con los numeros ---
+        [1] Encriptar por 
+        [2] Retornar 
+
+        """)
+
+
+    else:
+    os.system("cls")
+    print("\nopcion no reonocidad\n")
+    continue
