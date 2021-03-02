1ca16742874e1865b3c90e0d4a6171c1f6220018
ces95 <35697127+ces95@users.noreply.github.com>
2021-03-02 08:48:52 -0400

    Update demo-2.py

--- demo-2.py
+++ demo-2.py
@@ -2,7 +2,7 @@
 Realizado por:
 
 Cesar A. Diaz - 1075235
-Salma Marmol - 108
+Salma Marmol - 1085618
 
 """
 
@@ -21,6 +21,114 @@ from Crypto.Random import get_random_bytes
 
 
 #FUNCIONES
+
+#Metodos de Encriptado Asimetrico
+def Create_RSA_Key():
+    #crea la llave simetrica
+    key = Fernet.generate_key()
+
+    #escribe la llave simetrica a un file
+    k = open('symmetric.key','wb')
+    k.write(key)
+    k.close()
+
+    #crea las llaves publicas y privadas, de 2048 bits
+    (pubkey,privkey)= rsa.newkeys(2048)
+
+    #escribe la llave publica en un file
+    pukey= open( 'publickey.key','wb')
+    pukey.write(pubkey.save_pkcs1('PEM'))
+
+
+
+    pukey.close()
+
+    #escribe la llave privada en un file
+    prkey= open('privkey.key','wb')
+    prkey.write(privkey.save_pkcs1('PEM'))
+    prkey.close()
+
+def Encrypt_data_RSA():
+    #abre el file de la llave simetrica
+    simkey= open('symmetric.key','rb')
+    key = simkey.read()
+
+    #crea el cifrado
+    cipher = Fernet(key)
+
+    #abre el file para el cifrado
+    myfile = open('cripto','rb')    #El archivo en este caso se llama cripto
+    myfiledata = myfile.read()
+
+    #cifra los datos
+    encdata = cipher.encrypt(myfiledata)
+    edata = open('encrypted_file','wb')
+    edata.write(encdata)
+
+    print(encdata)
+
+    #abre el file de la llave publica
+    pkey = open('publickey.key','rb')
+    pkdata = pkey.read()
+
+    #carga el file
+    pubkey = rsa.PublicKey.load_pkcs1(pkdata)
+
+    #cifra el file de la llave simetrica con la llave publica
+    enckey = rsa.encrypt(key,pubkey)
+
+    #escribe la llave simetrica cifrada en un file
+    ekey = open('encrypted_key','wb')
+    ekey.write(enckey)
+
+    print(enckey)
+
+def Decrypt_data_RSA():
+
+    #carga la llave privada para descifrar la llave publica
+    prkey = open('privkey.key','rb')
+    pkey = prkey.read()
+    private_key = rsa.PrivateKey.load_pkcs1(pkey)
+
+    e = open('encrypted_key','rb')
+    ekey = e.read()
+
+    dpubkey = rsa.decrypt(ekey,private_key)
+
+    cipher= Fernet(dpubkey)
+
+    encrypted_data =open('encrypted_file','rb')
+    edata = encrypted_data.read()
+
+    decrypted_data = cipher.decrypt(edata)
+
+    print(decrypted_data.decode())
+
+#Metodos de Encriptado Simetrico
+
+def Encrypt_data_AES():
+    key = get_random_bytes(32) # Genera la llave
+    dataenc = 'Esto es tarea de criptografia!' # Son los datos
+
+    # === Cifrado ===
+
+    # Convierte los datos en bytes, usando .encode
+    data = dataenc.encode('utf-8')
+
+    #Crea el objecto de cifrado, y cifra los datos
+    cipherenc = AES.new(key, AES.MODE_CFB)
+    ciphbytes = cipherenc.encrypt(data)
+
+    # Aqui se presentan nuestros datos
+    iv = cipherenc.iv
+    ciphdata = ciphbytes
+
+    print(ciphdata)
+
+def Decrypt_data_AES():
+
+
+#Funcion MAIN - Para el menu del programa
 def main():
     ans = True
     while ans :
@@ -53,23 +161,26 @@ def main():
 
             if s=='1':
                 os.system('cls')
-                print("Encriptar por Algoritmo Simetrico")
+                print("\nEncriptar por Algoritmo Simetrico RSA\n")
+                Encrypt_data_AES()
                 os.system('pause')
 
             elif s=='2':
                 os.system('cls')
-                print("Encriptar por Algoritmo Asimetrico")
+                print("\nEncriptar por Algoritmo Asimetrico\n")
+                Create_RSA_Key()
+                Encrypt_data_RSA()
                 os.system('pause')
 
 
             elif s=='3':
                 os.system('cls')
-                print("Encriptar por Algoritmo basado en Matriz")
+                print("\nEncriptar por Algoritmo basado en Matriz\n")
                 os.system('pause')
 
             elif s=='4':
                 os.system('cls')
-                print('Retornando....\n\n')
+                print('\nRetornando....\n\n')
                 os.system('pause')
                 continue
             else:
@@ -94,22 +205,24 @@ def main():
 
             if d=='1':
                 os.system('cls')
-                print("\nDesencriptar por Algoritmo Simetrico")
+                print("\nDesencriptar por Algoritmo Simetrico\n")
+                Decrypt_data_AES()
                 os.system('pause')
 
             elif d=='2':
                 os.system('cls')
-                print("\nDesencriptar por Algoritmo Asimetrico")
+                print("\nDesencriptar por Algoritmo Asimetrico\n")
+                Decrypt_data_RSA()
                 os.system('pause')
 
             elif d=='3':
                 os.system('cls')
-                print("Desencriptar por Algoritmo basado en Matriz")
+                print("\nDesencriptar por Algoritmo basado en Matriz\n")
                 os.system('pause')
 
             elif d=='4':
                 os.system('cls')
-                print('Retornando....\n\n')
+                print('\nRetornando....\n\n')
                 os.system('pause')
                 continue
 
@@ -123,8 +236,6 @@ def main():
             os.system('cls')
             print('Saliendo del programa.... \n')
             break
-
-
 
         else:
             os.system('cls')
