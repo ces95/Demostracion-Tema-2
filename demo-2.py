0924fdb2ee4f299939ae83eaa818629ef25d5dd9
ces95 <35697127+ces95@users.noreply.github.com>
2021-03-01 22:54:03 -0400

    Update demo-2.py

--- demo-2.py
+++ demo-2.py
@@ -23,10 +23,10 @@ def matricial():
     print('')
 
 
-def main():
-    ans = ''
-    while ans :
-        print(
+
+ans = ''
+while ans==True:
+    print(
             """
             Menu de Inicio
             --- Seleccione una de las siguiente opciones con los numeros ---
@@ -36,11 +36,11 @@ def main():
 
             """)
         
-        ans = input("Que desea hacer? ")
+    ans = input("Que desea hacer? ")
         
-        if ans=="1":
-            os.system('cls')
-            print(
+    if ans=="1":
+        os.system('cls')
+        print(
             """
             Menu de Encriptado
             --- Seleccione una de las siguiente opciones con los numeros ---
@@ -50,16 +50,69 @@ def main():
             [4] Retornar 
 
             """)
+        s = input("Que desea hacer? ")
 
-        elif ans=='2':
+        if s=='1':
             os.system('cls')
 
-        elif ans=='3':
+            os.system('pause')
+
+        elif s=='2':
             os.system('cls')
 
-        elif ans=='4':
+            os.system('pause')
+
+
+        elif s=='3':
             os.system('cls')
 
+            os.system('pause')
+
+        elif s=='4':
+            os.system('cls')
+            print('Retornando....')
+            os.system('pause')
+            break
+        else:
+            os.system('cls')
+            print("\n   Opcion no reconocida...\n")
+            os.system('pause')
+            continue
+
+    elif ans=='2':
+        os.system('cls')
+        print(
+            """
+            Menu de Desencriptado
+            --- Seleccione una de las siguiente opciones con los numeros ---
+            [1] Desencriptar por Algoritmo Simetrico
+            [2] Desencriptar por Algoritmo Asimetrico
+            [3] Desencriptar por Algoritmo basado en Matriz
+            [4] Retornar 
+
+            """)
+        d = input("Que desea hacer? ")
+
+        if d=='1':
+            os.system('cls')
+
+            os.system('pause')
+
+        elif d=='2':
+            os.system('cls')
+
+            os.system('pause')
+
+        elif d=='3':
+            os.system('cls')
+
+            os.system('pause')
+
+        elif d=='4':
+            os.system('cls')
+            print('Retornando....')
+            os.system('pause')
+            break
 
         else:
             os.system('cls')
@@ -67,5 +120,19 @@ def main():
             os.system('pause')
             continue
 
+    elif ans=='3':
+        os.system('cls')
+        print('Saliendo del programa....')
+        os.system('pause')
+        break
+
+
+    else:
+        os.system('cls')
+        print("\n   Opcion no reconocida...\n")
+        os.system('pause')
+        continue
+    
+
 #INICIO DEL MENU DEL PROGRAMA
-main()
+
